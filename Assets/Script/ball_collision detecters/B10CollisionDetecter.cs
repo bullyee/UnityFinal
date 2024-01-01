cb54cd33408ee19f76ceffec0dddfdfde14261a6
bullyee <austingamer.cool@gmail.com>
2024-01-01 22:56:32 +0800

    Added EndGame Detection

--- Assets/Script/ball_collision detecters/B10CollisionDetecter.cs
+++ Assets/Script/ball_collision detecters/B10CollisionDetecter.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(Collider))]
 
 public class B10CollisionDetecter : MonoBehaviour
 {
