bc7d927bd7c960e7e030c02597180e75efcb5318
bullyee <61649844+bullyee@users.noreply.github.com>
2024-01-05 22:51:12 +0800

    Revert "FruitList EndGameMark"

--- Assets/Script/scorecounter.cs
+++ Assets/Script/scorecounter.cs
@@ -5,8 +5,8 @@ using UnityEngine.UI;
 
 public class scorecounter : MonoBehaviour
 {
-    public int score = 0;
-    public Image score_02,score_01,score_00,score_03;
+    int score = 0;
+    public Image score_02,score_01,score_00;
     public Sprite[] numberSprites;
     // Start is called before the first frame update
     void Start()
@@ -23,11 +23,9 @@ public class scorecounter : MonoBehaviour
     public void scoreadd(int s)
     {
         score+=s;
-        int d = score / 1000;
-        int a = (score-d*1000) / 100;
-        int b = (score-d*1000 - a * 100) / 10;
-        int c = score -d*1000 -a * 100 - b * 10;
-        score_03.sprite = numberSprites[d];
+        int a = score / 100;
+        int b = (score - a * 100) / 10;
+        int c = score - a * 100 - b * 10;
         score_02.sprite = numberSprites[a];
         score_01.sprite = numberSprites[b];
         score_00.sprite = numberSprites[c];        
